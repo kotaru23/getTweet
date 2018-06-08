feea0e985b35396d8727c10e002c4938999c5198
Asamin <CoffeeTaro@users.noreply.github.com>
2018-06-08 15:41:22 +0900

    [update] 引数に関する説明を追加

--- run.sh
+++ run.sh
@@ -11,4 +11,10 @@ function getFullPath () {
     fi
 }
 
+# 第1引数にTwitter API Keyのtomlファイルのパスを指定
+# 第2引数に取得したいscreen_name.txtを追加
+# 第3引数に出力先ディレクトリを指定
+# 第4引数に最後に取得したスクリーンネームを保存するためのファイルパスを指定
+# 第5引数にログファイルのパスを指定
+# それぞれ相対パスでもOK
 docker run --rm -v `getFullPath $1`:/app/key.toml -v `getFullPath $2`:/app/screen_name.txt -v `getFullPath $3`:/app/output/ -v `getFullPath $4`:/app/ScreenNameLatest.txt -v `getFullPath $5`:/app/getTweet.log geotaru/get-tweet
