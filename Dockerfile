53f2d69143807814273d7e5d37b32d9785ebdaf4
kotaru23 <28588608+kotaru23@users.noreply.github.com>
2019-03-03 23:59:28 +0900

    modify docker images

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,14 @@
-FROM python:3.7.2
-
-# -- Install Pipenv:
-RUN apt-get update -y && apt-get upgrade -y
+FROM python:3.7.2-alpine3.9
 
 ENV LC_ALL C.UTF-8
 ENV LANG C.UTF-8
 
-# -- Install Application into container:
-RUN set -ex && mkdir /app
-
 WORKDIR /app
-
 COPY requirements.txt requirements.txt
 
 # -- Install dependencies:
-RUN  pip install -r requirements.txt
+RUN  pip --no-cache-dir install -r requirements.txt
 
 COPY ./getTweet.py /app/getTweet.py
-CMD python getTweet.py -k /app/key.toml -s /app/screen_name.txt -o /app/output/
+
+CMD ["python", "getTweet.py", "-k", "/app/key.toml", "-s", "/app/screen_name.txt", "-o", "/app/output/"]
--- run.sh
+++ run.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-function getFullPath () {
-    # パスを絶対パスにする
-    if [ ${1:0:1} != "/" ]; then
-        # 相対パスが引数に入力されたとき(1文字目が"/"なら)
-        echo `pwd`/$1
-    else
-        # 絶対パスが引数に入力されたとき
-        echo $1
-    fi
-}
-
 # 第1引数にTwitter API Keyのtomlファイルのパスを指定
 # 第2引数に取得したいscreen_name.txtを追加
 # 第3引数に出力先ディレクトリを指定
-# 第4引数に最後に取得したスクリーンネームを保存するためのファイルパスを指定
-# 第5引数にログファイルのパスを指定
-# それぞれ相対パスでもOK
-docker run --rm -v `getFullPath $1`:/app/key.toml -v `getFullPath $2`:/app/screen_name.txt -v `getFullPath $3`:/app/output/ -v `getFullPath $4`:/app/ScreenNameLatest.txt -v `getFullPath $5`:/app/getTweet.log geotaru/get-tweet
+docker run --rm \
+    -v $1:/app/key.toml \
+    -v $2:/app/screen_name.txt \
+    -v $3:/app/output/ \
+    get-tweet
